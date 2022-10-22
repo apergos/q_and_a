eb59e68b5016eb60366482e17b79088f0464f6f4
Ariel T. Glenn <ariel@wikimedia.org>
2022-10-22 16:11:12 +0300

    Baby project to manage q&a for starting engineers
    first commit, guaranteed to be crap

---
+++ README.txt
@@ -0,0 +1,21 @@
+What this is, how it works
+
+Right now you do the following:
+
+* add stuff to the entries in QansA.yaml
+* run q_and_a.py > new-html-file.html
+* check that you like the results
+* profit
+
+Later we should:
+
+separate each topic into its own file
+have a file that lists the topics we want in the order we want, by filename I guess
+yeah that's likely better.
+then read them all into one content and continue as we do
+
+
+
+
+
+
---
+++ q_and_a.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import yaml
+
+
+def get_file_contents(path):
+    '''return contents of a file at the given path'''
+    with open(path, "r") as infile:
+        text = infile.read()
+        return text
+
+
+def put_html_header():
+    '''write a header for an html doc to stdout'''
+    print('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">')
+    print('<html>')
+    # FIXME this should not be hardcoded
+    print('<body bgcolor="#ffffff">')
+
+
+def put_html_footer():
+    '''write a footer for an html doc to stdout'''
+    print('</body>')
+    print('</html>')
+
+
+def put_intro(content):
+    '''write the intro text for the q & a doc to stdout'''
+    print(content)
+
+
+def put_topic_start(topic):
+    '''write the topic section header with markup to stdout'''
+    # FIXME this should not be hardcoded
+    print('<details>\n<summary>' + topic + '</summary>\n\n<dl>\n')
+
+
+def put_topic_end():
+    '''write the close topic markup to stdout'''
+    # FIXME this should not be hardcoded
+    print('</dl>\n</details>\n')
+
+
+def put_entry(entry, templ):
+    '''write out a question with formatting to stdout'''
+    print(templ % { "question": entry['question'],
+                    "answer": entry['answer'],
+                    "readings": entry['readings'],
+                    "exercises": entry['exercises']})
+
+    
+def do_main():
+    '''entry point'''
+    template = get_file_contents('QandA.templ')
+    intro = get_file_contents('Intro.html')
+    topics = yaml.safe_load(get_file_contents('QandA.yaml'))
+    put_html_header()
+    put_intro(intro)
+    for topic in topics:
+        put_topic_start(topic)
+        for entry in topics[topic]:
+            put_entry(entry['entry'], template)
+        put_topic_end()
+    put_html_footer()
+
+
+if __name__ == '__main__':
+    do_main()
+
